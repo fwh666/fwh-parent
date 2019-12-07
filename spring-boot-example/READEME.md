55d6c3ec32037e8b7220a3061c8bfe5e8ab47042
fuwenhao-mac <fuwenhao594@163.com>
2019-12-07 22:48:29 +0800

    新增：springboot-demo模块

---
+++ spring-boot-example/READEME.md
@@ -0,0 +1,25 @@
+# 记录：
+
+springboot1.0版本和springboot2.0版本比对，以及相关依赖引用，方法使用。
+
+## 模块记录
+   ### Redis
+   - redisTemplate的增删改查、失效时间、选择库
+    - 数据类型：
+        - string
+        - map
+        - hash
+        - set
+        - list
+   ### OAuth
+   - 认证
+   - 权限
+## TODO-开发
+   - shiro
+   - mybatis-plus
+   - docker
+   - rabbitmq
+   - swagger
+   - mail
+   - fastDfs
+   - mongodb
---
+++ spring-boot-example/spring-boot-redis/src/main/java/com/fuwenhao/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.fuwenhao.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Method;
+
+/**
+ * redis配置
+ *
+ * @author fwh
+ * @email fuwenhao594@163.com
+ * @date 2019/12/7 下午10:40
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public KeyGenerator keyGenerator() {
+        return new KeyGenerator() {
+            @Override
+            public Object generate(Object target, Method method, Object... params) {
+                StringBuilder sb = new StringBuilder();
+                sb.append(target.getClass().getName());
+                sb.append(method.getName());
+                for (Object obj : params) {
+                    sb.append(obj.toString());
+                }
+                return sb.toString();
+            }
+        };
+    }
+}
