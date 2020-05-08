8e95786ad6b20711c5fd34a7a0f34a01b022a5ed
fuwenhao-mac <fuwenhao594@163.com>
2020-05-08 11:39:34 +0800

    新增：MongoDB范围查询

--- fwh-mongodb/src/main/java/com/fuwenhao/bean/User.java
+++ fwh-mongodb/src/main/java/com/fuwenhao/bean/User.java
@@ -1,9 +1,7 @@
 package com.fuwenhao.bean;
 
 import lombok.Data;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
@@ -13,13 +11,12 @@ import java.util.Date;
  * @date 2020/5/7 5:05 下午
  */
 @Data
-@Document(collation = "fwh")
+//@Document(collation = "fwh")
 public class User {
     @Id
     private long id;
     private String name;
     private int age;
     private boolean sex;
-    @CreatedDate
     private Date createTime;
 }
