b70bbd6383ff3142e99183f7570b32d71cd40c90
fuwenhao-mac <fuwenhao594@163.com>
2020-05-27 16:31:41 +0800

    修改：针对参数值流控

--- fwh-sentinel/src/main/java/com/fuwenhao/controller/SentinelController.java
+++ fwh-sentinel/src/main/java/com/fuwenhao/controller/SentinelController.java
@@ -1,7 +1,9 @@
 package com.fuwenhao.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -32,5 +34,18 @@ public class SentinelController {
             System.out.println(forEntity.getBody() + i + " 次");
         }
         return "访问资源：sourceA";
+    }
+
+    /**
+     * 测试参数-热点限制
+     *
+     * @param paramName
+     * @return java.lang.String
+     * @author fwh [2020/5/22 && 5:25 下午]
+     */
+    @GetMapping("sourceC/{paramName}")
+//    @SentinelResource()
+    public String sourceC(@PathVariable("paramName") String paramName) {
+        return paramName;
     }
 }
