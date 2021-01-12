e14f20295f85334d8c5c444af218b7cd6a0988ed
fuwenhao-mac <fuwenhao594@163.com>
2021-01-12 15:16:19 +0800

    新增：锁Demo

---
+++ fwh-base/fwh-lock/src/test/java/LockTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+/**
+ * @author fwh
+ * @email fuwenhao594@163.com
+ * @date 2021/1/12 2:36 下午
+ */
+public class LockTest {
+    /**
+     * volatile 关键字
+     * 网址：https://www.infoq.cn/article/ftf-java-volatile/
+     *
+     * @param
+     * @return void
+     * @author fwh [2021/1/12 && 3:10 下午]
+     */
+    @Test
+    public void volatileTest() {
+
+    }
+
+    /**
+     * CasABA问题
+     * 网址： https://www.cnblogs.com/549294286/p/3766717.html
+     *
+     * @param
+     * @return void
+     * @author fwh [2021/1/12 && 3:11 下午]
+     */
+    @Test
+    public void CasAba() {
+
+    }
+}
