ca36664cc56b655ac56f7539751e8d97f4e55d3d
fuwenhao-mac <fuwenhao594@163.com>
2021-01-19 19:33:30 +0800

    修改：去除无效依赖

--- fwh-cloud/fwh-web/src/main/java/club/fuwenhao/WebApplication.java
+++ fwh-cloud/fwh-web/src/main/java/club/fuwenhao/WebApplication.java
@@ -3,7 +3,7 @@ package club.fuwenhao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author fwh
@@ -12,7 +12,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
+//@EnableFeignClients
 public class WebApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class,args);
