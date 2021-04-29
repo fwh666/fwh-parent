d91a3bb44522cd08c4faf29d137d60a20098c4c7
ext.fuwenhao <ext.fuwenhao@jd.com>
2021-04-29 17:12:34 +0800

    新增：aop日志切面输出

--- fwh-cloud/fwh-web/src/main/java/club/fuwenhao/controller/TestController.java
+++ fwh-cloud/fwh-web/src/main/java/club/fuwenhao/controller/TestController.java
@@ -1,10 +1,13 @@
 package club.fuwenhao.controller;
 
+import club.fuwenhao.aspect.WebLog;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalTime;
 
 /**
  * @author fwh
@@ -13,10 +16,10 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 @RequestMapping("web")
+@WebLog(description = "测试控制层")
 public class TestController {
     @GetMapping("hello")
-    public String hello(HttpServletRequest request) {
-        final String method = request.getMethod();
-        return "方法："+method+",hello:" + request.getParameterMap().toString();
+    public String hello(HttpServletRequest request, @RequestParam("argName") String argName) {
+        return "方法：" + request.getMethod() + "参数argName为：" + argName + ",hello:" + LocalTime.now();
     }
 }
