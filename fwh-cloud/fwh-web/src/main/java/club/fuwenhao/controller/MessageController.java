ba469f657dd096a1eef192dad5f3b9a8c267c041
ext.fuwenhao <ext.fuwenhao@jd.com>
2021-05-18 16:09:34 +0800

    新增:发送邮件功能

--- fwh-cloud/fwh-web/src/main/java/club/fuwenhao/controller/MessageController.java
+++ fwh-cloud/fwh-web/src/main/java/club/fuwenhao/controller/MessageController.java
@@ -1,5 +1,10 @@
 package club.fuwenhao.controller;
 
+import club.fuwenhao.aspect.WebLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author: fwh
  * @date: 2021-05-18 14:27
  **/
+@WebLog
 @RestController
 public class MessageController {
+    @Autowired
+    private JavaMailSender mailSender;
+
 
     @PostMapping("/messageCheck")
     public String messageCheck(@RequestParam("phone") String phone) {
@@ -19,5 +28,29 @@ public class MessageController {
         //短信验证码校验
         //校验通过
         return null;
+    }
+
+    /**
+     * 发送邮件
+     *
+     * @param
+     * @return java.lang.Boolean
+     * @author fuwenhao
+     * @date 2021/5/18 3:57 下午
+     */
+    @PostMapping("/mailSend")
+    public Boolean mailSend() {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("test@163.com");
+        message.setTo("test@qq.com");
+        message.setSubject("it is a test for spring boot");
+        message.setText("你好，我是小黄，我正在测试发送邮件。");
+        try {
+            mailSender.send(message);
+        } catch (MailException e) {
+            e.getMessage();
+            return false;
+        }
+        return true;
     }
 }
